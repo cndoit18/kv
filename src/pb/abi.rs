32832e8165b238317572bf581028dc75a820a5b6
cndoit18 <cndoit18@outlook.com>
2024-05-20 00:16:47 +0800

    add examples

--- src/pb/abi.rs
+++ src/pb/abi.rs
@@ -35,8 +35,8 @@ pub struct Hmget {
 pub struct Hset {
     #[prost(string, tag = "1")]
     pub table: ::prost::alloc::string::String,
-    #[prost(message, optional, boxed, tag = "2")]
-    pub pair: ::core::option::Option<::prost::alloc::boxed::Box<Kvpair>>,
+    #[prost(message, optional, tag = "2")]
+    pub pair: ::core::option::Option<Kvpair>,
 }
 /// 往 table 中存一组 kvpair
 /// 如果 table 不存在就创建这个 table
@@ -93,7 +93,7 @@ pub struct Hmexist {
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct CommandRequest {
-    #[prost(oneof = "command_request::RequestData", tags = "1, 2, 3")]
+    #[prost(oneof = "command_request::RequestData", tags = "1, 2, 3, 4, 5, 6, 7, 8, 9")]
     pub request_data: ::core::option::Option<command_request::RequestData>,
 }
 /// Nested message and enum types in `CommandRequest`.
@@ -108,30 +108,8 @@ pub mod command_request {
         Hgetall(super::Hgetall),
         #[prost(message, tag = "3")]
         Hmget(super::Hmget),
-    }
-}
-/// 返回数据
-#[derive(PartialOrd)]
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Value {
-    #[prost(oneof = "value::Value", tags = "1, 2, 3, 4, 5, 6, 7, 8, 9")]
-    pub value: ::core::option::Option<value::Value>,
-}
-/// Nested message and enum types in `Value`.
-pub mod value {
-    #[derive(PartialOrd)]
-    #[allow(clippy::derive_partial_eq_without_eq)]
-    #[derive(Clone, PartialEq, ::prost::Oneof)]
-    pub enum Value {
-        #[prost(message, tag = "1")]
-        Hget(super::Hget),
-        #[prost(message, tag = "2")]
-        Hgetall(super::Hgetall),
-        #[prost(message, tag = "3")]
-        Hmget(super::Hmget),
         #[prost(message, tag = "4")]
-        Hset(::prost::alloc::boxed::Box<super::Hset>),
+        Hset(super::Hset),
         #[prost(message, tag = "5")]
         Hmset(super::Hmset),
         #[prost(message, tag = "6")]
@@ -144,6 +122,32 @@ pub mod value {
         Hmexist(super::Hmexist),
     }
 }
+/// 返回数据
+#[derive(PartialOrd)]
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Value {
+    #[prost(oneof = "value::Value", tags = "1, 2, 3, 4, 5")]
+    pub value: ::core::option::Option<value::Value>,
+}
+/// Nested message and enum types in `Value`.
+pub mod value {
+    #[derive(PartialOrd)]
+    #[allow(clippy::derive_partial_eq_without_eq)]
+    #[derive(Clone, PartialEq, ::prost::Oneof)]
+    pub enum Value {
+        #[prost(string, tag = "1")]
+        String(::prost::alloc::string::String),
+        #[prost(bytes, tag = "2")]
+        Binary(::prost::bytes::Bytes),
+        #[prost(int64, tag = "3")]
+        Integer(i64),
+        #[prost(double, tag = "4")]
+        Float(f64),
+        #[prost(bool, tag = "5")]
+        Bool(bool),
+    }
+}
 /// 返回的 kvpair
 #[derive(PartialOrd)]
 #[allow(clippy::derive_partial_eq_without_eq)]
@@ -151,8 +155,8 @@ pub mod value {
 pub struct Kvpair {
     #[prost(string, tag = "1")]
     pub key: ::prost::alloc::string::String,
-    #[prost(message, optional, boxed, tag = "2")]
-    pub value: ::core::option::Option<::prost::alloc::boxed::Box<Value>>,
+    #[prost(message, optional, tag = "2")]
+    pub value: ::core::option::Option<Value>,
 }
 /// 服务器的响应
 #[derive(PartialOrd)]
