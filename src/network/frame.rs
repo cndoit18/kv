2b9d1fbaf92944fa16238e7c87795959665e443d
cndoit18 <cndoit18@outlook.com>
2024-06-06 02:11:43 +0800

    add tokio

--- src/network/frame.rs
+++ src/network/frame.rs
@@ -1,13 +1,14 @@
-use crate::{KvError, CommandResponse, CommandRequest};
+use crate::{CommandRequest, CommandResponse, KvError};
 use anyhow::Result;
 use bytes::{Buf, BufMut, BytesMut};
 use flate2::{read::GzDecoder, write::GzEncoder, Compression};
 use prost::Message;
 use std::io::{Read, Write};
+use tokio::io::{AsyncRead, AsyncReadExt};
 
 pub const LEN_LEN: usize = 4;
 const MAX_FRAME: usize = 2 * 1024 * 1024 * 1024;
-pub(super) const COMPRESSION_LIMIT: usize = 1436;
+const COMPRESSION_LIMIT: usize = 1436;
 const COMPRESSION_BIT: usize = 1 << 31;
 
 pub trait FrameCoder
@@ -69,6 +70,114 @@ fn decode_header(header: usize) -> (usize, bool) {
     (len, compressed)
 }
 
+pub async fn read_frame<S>(stream: &mut S, buf: &mut BytesMut) -> Result<()>
+where
+    S: AsyncRead + Unpin + Send,
+{
+    let header = stream.read_u32().await? as usize;
+    let (len, _compressed) = decode_header(header);
+    buf.reserve(LEN_LEN + len);
+    buf.put_u32(header as _);
+    unsafe { buf.advance_mut(len) };
+    stream.read_exact(&mut buf[LEN_LEN..]).await?;
+    Ok(())
+}
 
 impl FrameCoder for CommandRequest {}
 impl FrameCoder for CommandResponse {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{CommandRequest, CommandResponse, Value};
+    use bytes::{Bytes, BytesMut};
+
+    #[test]
+    fn command_request_encode_decode_should_work() {
+        let mut buf = BytesMut::new();
+
+        let cmd = CommandRequest::new_hdel("t1", "k1");
+        cmd.encode_frame(&mut buf).unwrap();
+
+        // 最高位没设置
+        assert_eq!(is_compressed(&buf), false);
+
+        let cmd1 = CommandRequest::decode_frame(&mut buf).unwrap();
+        assert_eq!(cmd, cmd1);
+    }
+
+    #[test]
+    fn command_response_encode_decode_should_work() {
+        let mut buf = BytesMut::new();
+
+        let values: Vec<Value> = vec![1.into(), "hello".into(), b"data".into()];
+        let res: CommandResponse = values.into();
+        res.encode_frame(&mut buf).unwrap();
+
+        // 最高位没设置
+        assert_eq!(is_compressed(&buf), false);
+
+        let res1 = CommandResponse::decode_frame(&mut buf).unwrap();
+        assert_eq!(res, res1);
+    }
+
+    #[test]
+    fn command_response_compressed_encode_decode_should_work() {
+        let mut buf = BytesMut::new();
+
+        let value: Value = Bytes::from(vec![0u8; COMPRESSION_LIMIT + 1]).into();
+        let res: CommandResponse = value.into();
+        res.encode_frame(&mut buf).unwrap();
+
+        // 最高位设置了
+        assert_eq!(is_compressed(&buf), true);
+
+        let res1 = CommandResponse::decode_frame(&mut buf).unwrap();
+        assert_eq!(res, res1);
+    }
+
+    fn is_compressed(data: &[u8]) -> bool {
+        if let &[v] = &data[..1] {
+            v >> 7 == 1
+        } else {
+            false
+        }
+    }
+
+    struct DummyStream {
+        buf: BytesMut,
+    }
+
+    impl AsyncRead for DummyStream {
+        fn poll_read(
+            self: std::pin::Pin<&mut Self>,
+            _cx: &mut std::task::Context<'_>,
+            buf: &mut tokio::io::ReadBuf<'_>,
+        ) -> std::task::Poll<std::io::Result<()>> {
+            // 看看 ReadBuf 需要多大的数据
+            let len = buf.capacity();
+
+            // split 出这么大的数据
+            let data = self.get_mut().buf.split_to(len);
+
+            // 拷贝给 ReadBuf
+            buf.put_slice(&data);
+
+            // 直接完工
+            std::task::Poll::Ready(Ok(()))
+        }
+    }
+    #[tokio::test]
+    async fn read_frame_should_work() {
+        let mut buf = BytesMut::new();
+        let cmd = CommandRequest::new_hdel("t1", "k1");
+        cmd.encode_frame(&mut buf).unwrap();
+        let mut stream = DummyStream { buf };
+
+        let mut data = BytesMut::new();
+        read_frame(&mut stream, &mut data).await.unwrap();
+
+        let cmd1 = CommandRequest::decode_frame(&mut data).unwrap();
+        assert_eq!(cmd, cmd1);
+    }
+}
